cb5b0ac319ae766d997b643da90f846b687ee54c
bombs <bluewhale8202@gmail.com>
2020-07-01 17:49:02 +0000

    Fixed order in validation

--- data.py
+++ data.py
@@ -196,7 +196,8 @@ class QueryDataset(Dataset):
 
     def __getitem__(self, idx):
         l = list(self.collabs[idx])
-        shuffle(l)
+        if self.split == 'train':
+            shuffle(l)
         collab = torch.tensor(l, dtype=torch.long)
         label = torch.tensor(self.labels[idx], dtype=torch.long)
         if self.equally_handle_foreign_authors:
